9384baf5973ed0361437642489d5903021365678
tommyrodr <tlr0039@uah.edu>
2025-09-25 22:01:54 -0500

    Continued Creation of the Star concrete class.

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using ChargerAstronomyShared.Domain.Equatorial;
 using ChargerAstronomyShared.Domain.Horizontal;
+using NUnit.Framework.Internal.Commands;
 
 
 
@@ -27,10 +28,34 @@ namespace Stargazers.Unity
 
         // Set Hipparcs ID if available, otherwise use equatorial star's Id, otherwise 0
         public int HipparcosId => starHz?.HipparcosId ?? starEq?.HipparcosId ?? 0;
+
         // Use star name from the horizontal data if available, otherwise use the catalogs proper name, otherwise Unamed Star.
         public string StarName => string.IsNullOrWhiteSpace(starHz?.StarName)
                                     ? (string.IsNullOrWhiteSpace(starEq?.ProperName) ? "Unnamed Star" : starEq.ProperName)
                                     : starHz.StarName);
+        // Apparent brightness of the star
+        public float Magnitude { get { return starHz?.Magnitude; } }
+
+        // World Position
+        public Vector3 Position3D { get; private set; }
+        
+        //Screen-space position helper
+        public Vector2 Position2D { get; private set; }
+
+        public void FromHorizontal(HorizontalStar hstar, float drawnDistance = 74f)
+        {
+            starHz = hstar;
+            horizontalBody = hstar;
+            equatorialBody = null;
+            DrawnDistance = drawnDistance;
+
+        }
+
+
+
+
+
+
 
 
     }

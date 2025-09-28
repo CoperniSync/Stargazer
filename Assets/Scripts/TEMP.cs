448c58f3099d59993f9897fb89141a1e659e0fcc
joshjohner <josh.johner@gmail.com>
2025-09-28 17:32:48 -0500

    Revert "Automated DLL for Engine."

--- Assets/Scripts/TEMP.cs
+++ Assets/Scripts/TEMP.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using ChargerAstronomyEngine;
 
 public class TEMP : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        var test = new Class1();
+        Debug.Log(test.Test());
     }
 
     // Update is called once per frame

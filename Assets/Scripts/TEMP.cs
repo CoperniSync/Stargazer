0bab968997f87903dc8debdf9201acace5d01287
josh.johner <josh.johner@gmail.com>
2025-09-28 17:43:16 -0500

    Removed symlinks

--- Assets/Scripts/TEMP.cs
+++ Assets/Scripts/TEMP.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
-using ChargerAstronomyEngine;
 
 public class TEMP : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var test = new Class1();
-        Debug.Log(test.Test());
+
     }
 
     // Update is called once per frame

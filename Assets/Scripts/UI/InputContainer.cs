35577fa90f518ed9cdf4d1d99883468df85cfb0e
Morgana2003 <161104896+Morgana2003@users.noreply.github.com>
2025-09-28 15:39:59 -0500

    Create Container for Input
    a container for holding user inputs

---
+++ Assets/Scripts/UI/InputContainer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.UI
+{
+
+
+    public class InputContainer : MonoBehaviour
+    {
+        // make a singleton
+        public static InputContainer container;
+        private void Awake()
+        {
+            if (container == null)
+                container = this;
+        }
+
+        //location
+        public int LongitudeDeg {  get; set; }
+        public int LatitudeDeg { get; set; }
+        public int LongitudeMin { get; set; }
+        public int LatitudeMin { get; set; }
+
+        //time
+        public DateTime Time { get; set; }
+
+        //labels
+        public bool AzimuthLabel { get; set; }
+        public bool EquatorialLabel { get; set; }
+        public bool ConstellationLabel { get; set; }
+
+        //space objects
+        public bool ConstellationsToggle {  get; set; }
+        public bool MessierToggle { get; set; }
+
+        //speed
+        public uint DaysPerSec {  get; set; }
+        public uint MinPerSec { get; set; }
+        public uint SecPerSec { get; set; }
+
+    }
+}

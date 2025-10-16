d98cea8cb164fa22d2a3426a30ba8d73e266902e
Morgana2003 <161104896+Morgana2003@users.noreply.github.com>
2025-10-16 15:39:00 -0500

    Revert "Merge branch 'dev' into UI"
    This reverts commit 7ac260bf8377aa119f2fc007ca2f690e22eea71f, reversing
    changes made to 347b31864a6c6d296aadb3b27254ff5d2f7fdc76.

--- Assets/Scripts/Core/GameLoop.cs
+++ Assets/Scripts/Core/GameLoop.cs
@@ -1,19 +1,44 @@
+using ChargerAstronomyShared.Contracts.Models;
+using System.Collections;
 using UnityEngine;
 
 public class UnityInterface : MonoBehaviour
 {
     //class that holds the core game loop
 
-    Core core;
+    PageRequest starPageRequest; //used for requesting Stars
+    PageResult starPageResult;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        core = new Core();
+        starPageRequest = new();
     }
 
     // Update is called once per frame
     void Update()
     {
-        core.Update(Time.deltaTime);
+        UpdateSimulation(Time.deltaTime);
+    }
+
+
+    public void UpdateSimulation(float deltaTime)
+    {
+    /// <summary>
+    /// Main method to handle all things that need to be done in a frame
+    /// </summary>
+    
+
+    }
+
+    IEnumerator GetStars()
+    {
+        /// <summary>
+        /// coroutine for getting all the stars
+        /// </summary>
+        /// 
+
+        return null;
     }
 }

7e6d58924b6d4b8258add331feae3cc49cf8df6e
Morgana2003 <161104896+Morgana2003@users.noreply.github.com>
2025-11-08 17:10:22 -0600

    Add Horizon Line
    The horizon line appears and becomes more or less transparent based upon the camera position

---
+++ Assets/Scripts/UI/HorizonFader.cs
@@ -0,0 +1,33 @@
+using Assets.Scripts.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+using System;
+
+/// <summary>
+/// Class that controls the transparency of the horizion line
+/// </summary>
+public class HorizonFader : MonoBehaviour
+{
+
+    public bool enabled = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enabled)
+        {
+            Color color = GetComponent<Renderer>().material.color;
+            color.a = Math.Clamp((Vector3.Angle(Vector3.down, InputContainer.Container.RotationVector) - 60f) / 720f, 0f, 0.3f);
+            GetComponent<Renderer>().material.SetColor("_Color", color);
+        }
+        else
+        {
+            Color color = GetComponent<Renderer>().material.color;
+            color.a = Math.Clamp((Vector3.Angle(Vector3.down, InputContainer.Container.RotationVector) - 60f) / 720f, 0f, 0.3f);
+            GetComponent<Renderer>().material.SetColor("_Color", color);
+        }
+    }
+
+    
+}

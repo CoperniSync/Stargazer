988d59d76daa201b22fb1212ac6701e25901e072
tommyrodr <tlr0039@uah.edu>
2025-11-09 16:03:48 -0600

    fix

--- Assets/Scripts/CelestialBodies/MessierObject.cs
+++ Assets/Scripts/CelestialBodies/MessierObject.cs
@@ -48,7 +48,7 @@ namespace Assets.Scripts.CelestialBodies
 
             GameObject messierObjectPrefab = Resources.Load<GameObject>("Prefabs/MessierObject");
 
-            go = Object.Instantiate(messierObjectPrefab, Position3D, Quaternion.identity);
+            //go = Object.Instantiate(messierObjectPrefab, Position3D, Quaternion.identity);
             go.name = $"Messier_{MessierId}";
         }
 
